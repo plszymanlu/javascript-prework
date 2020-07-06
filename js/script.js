1e1119aa0c1dfd131ef5051128612e58cec272e2
lukasz szymanski <plszymanlu@gmail.com>
2020-07-06 23:41:01 +0200

    zmienne gry

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+var computerMove;
+computerMove = 'kamień';
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+printMessage('Zagrałem ' + 'nożyce' + '! Jeśli Twój ruch to kamień, to wygrywasz!');
