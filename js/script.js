457e016eaab3c05f21ecc3f306c781da5b1cca6a
lukasz szymanski <plszymanlu@gmail.com>
2020-07-09 00:30:10 +0200

    gra

--- js/script.js
+++ js/script.js
@@ -7,4 +7,23 @@ playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
 var randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 2);
+console.log('wylosowana liczba to: ' + randomNumber);
 printMessage('Wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+} else {
+    computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+if (randomNumber == '2') {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+if (randomNumber == '3') {
+    computerMove = 'nożyce';
+} else {
+    computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
